87c171d0b3d375e6b07c10f70a0c91695057880c
William <willspecker@gmail.com>
2020-10-10 20:30:20 +0200

    Adding updated fizz_buzz file that passes rspec test for 'fizz' quality

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,8 +1,8 @@
 require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
-    it 'returns 1 if number is 1' do
-        expect(fizz_buzz(1)).to eq 1
+    it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(3)).to eq 'fizz'
     end
     
 end
