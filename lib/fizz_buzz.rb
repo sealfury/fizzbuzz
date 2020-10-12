640e26e761cab6fb9379b59156d8538c9fe026d3
William <willspecker@gmail.com>
2020-10-12 18:01:36 +0200

    Creating simple fizzbuzz application and rspec to test it *need to work on sad paths further*

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,8 +1,16 @@
-
 def fizz_buzz(number)
-    if number % 3 == 0
+    if is_divisible_by(number, 15)
+        'fizzbuzz'
+    elsif is_divisible_by(number, 5)
+        'buzz'
+    elsif is_divisible_by(number, 3)
         'fizz'
     else
         number
     end
 end
+
+def is_divisible_by(number, divisor)
+    number % divisor == 0
+end
+
